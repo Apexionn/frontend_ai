57914a6803f27583014d23c7bdd432da93a2d55c
Lintang <saya.lintang@gmail.com>
2025-12-10 20:40:13 +0700

    Update Code

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ export default function App() {
   return (
     <div className="max-w-3xl mx-auto py-10">
       <h1 className="text-3xl font-bold text-center mb-6">
-        🌾 Prediksi Hasil Panen Padi
+        PATANI - Prediksi Hasil Panen Padi
       </h1>
 
       <FormPredict onResult={handleResult} />
